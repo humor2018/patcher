1804ef28ab17683ac47f11a897b274b01b113bd5
humor2008 <12211800@qq.com>
2017-09-06 09:47:05 +0800

    by humor
    Change-Id: I3fbfdadd14dae9555c05ad28deee8cadc33841e3

--- patcher.sh
+++ patcher.sh
@@ -44,13 +44,13 @@ patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/framework
 patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base3.patch
 patch -d frameworks/base			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-base4.patch
 patch -d frameworks/native			-p1 -s -N --no-backup-if-mismatch < $SULTAN/frameworks-native0.patch
-patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-LockClock0.patch
-patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Settings0.patch
+#patch -d packages/apps/LockClock		-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-LockClock0.patch
+#patch -d packages/apps/Settings			-p1 -s -N --no-backup-if-mismatch < $SULTAN/packages-apps-Settings0.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core0.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core1.patch
 patch -d system/core				-p1 -s -N --no-backup-if-mismatch < $SULTAN/system-core2.patch
 patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm0.patch
-patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm1.patch
+#patch -d vendor/cm				-p1 -s -N --no-backup-if-mismatch < $SULTAN/vendor-cm1.patch
 
 ### zx2c4's patches
 $ZX2C4/wireguard-fetch.sh || rm -f $ZX2C4/wireguard-src.patch
